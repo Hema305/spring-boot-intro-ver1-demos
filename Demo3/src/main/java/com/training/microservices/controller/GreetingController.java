602c40fb6812bec49644ec39d073f1bdb56d7e60
Satyen Singh  🇮🇳 <satyensingh@MacBookPro.local>
2018-01-27 13:44:11 +0530

    First Commit

---
+++ Demo3/src/main/java/com/training/microservices/controller/GreetingController.java
@@ -0,0 +1,38 @@
+package com.training.microservices.controller;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.training.microservices.bean.Greet;
+
+/*
+@Controller
+@ResponseBody
+*/
+@RestController
+public class GreetingController {
+	
+	//private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@RequestMapping("/greet")
+	Greet greet(){
+		return new Greet("Hello World!");
+	}
+	
+    @RequestMapping("/greeting")
+    @ResponseBody
+    public HttpEntity<Greet> greeting(@RequestParam(value = "name", required = false, defaultValue = "HATEOAS") String name) {
+        Greet greet = new Greet("Hello " + name);
+        greet.add(linkTo(methodOn(GreetingController.class).greeting(name)).withSelfRel());
+        return new ResponseEntity<Greet>(greet, HttpStatus.OK);
+    }
+	
+}
